206acedbaba947fa79eb4b619f977c86dceaae96
Sven <sven@mutzl.org>
2017-12-02 15:28:59 +0000

    Polonies promises

--- module/poloniex.js
+++ module/poloniex.js
@@ -5,7 +5,32 @@ const secret = process.env.POLONIEX_SECRET; // API Private Key
 const poloniex = new Poloniex(key, secret);
 
 module.exports = {
-    buy: poloniex.buy,
-    sell: poloniex.sell,
+    buy: buy,
+    sell: sell,
     balances: poloniex.returnBalances
 };
+
+
+function buy(pair, limit, quantity, fillorkill, p, q){
+    return new Promise((resolve,reject)=>{
+        poloniex.buy(pair, limit, quantity, 1, false, false, function(error, data){
+            if(error){
+                reject(Error(error.message));
+            } else {
+                resolve(data);
+            }
+        });
+    });
+}
+
+function sell(pair, limit, quantity, fillorkill, p, q){
+    return new Promise((resolve,reject)=>{
+        poloniex.sell(pair, limit, quantity, 1, false, false, function(error, data){
+            if(error){
+                reject(Error(error.message));
+            } else {
+                resolve(data);
+            }
+        });
+    });
+}
