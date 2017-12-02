5da9de42ead26770e962b0f57f999f1cf097ca00
Sven <sven@mutzl.org>
2017-12-02 16:07:14 +0000

    Cleanup

--- bot.js
+++ bot.js
@@ -4,32 +4,34 @@ let mysql_modul = require('./module/mysql_modul'),
 
 var job = timer('*/1 * * * * *', () => {
     mysql_modul.get_choice()
-        .then(results => {
-            if (results[0].status == 0) {
-                exchange.balances()
-                    .then((balances) => {
-                        if (balances.BTC > 0.001) {
-                            return exchange.buy('BTC_ETH', results[0].btceth, (balances.BTC / results[0].btceth), 1, 0, 0)
-                                .then(() => exchange.buy('ETH_ZEC', results[0].ethzec, (balances.ETH / results[0].ethzec), 1, 0, 0))
-                                .then(() => exchange.sell('BTC_ZEC', results[0].btczec, balances.ZEC, 1, 0, 0));
-                        } else if (balances.ETH > 0.02) {
+        .then((results) => workToDo(results))
+        .then(results => exchange.balances())
+        .then((balances) => {
+            if (balances.BTC > 0.001) {
+                return exchange.buy('BTC_ETH', results[0].btceth, (balances.BTC / results[0].btceth), 1, 0, 0)
+                    .then(() => exchange.buy('ETH_ZEC', results[0].ethzec, (balances.ETH / results[0].ethzec), 1, 0, 0))
+                    .then(() => exchange.sell('BTC_ZEC', results[0].btczec, balances.ZEC, 1, 0, 0));
+            } else if (balances.ETH > 0.02) {
 
-                            return exchange.buy('ETH_ZEC', results[0].ethzec, (balances.ETH / results[0].ethzec), 1, 0, 0)
-                                .then(() => exchange.sell('BTC_ZEC', results[0].btczec, balances.ZEC, 1, 0, 0));
-                        } else if (balances.ZEC > 0.05) {
-                            return exchange.sell('BTC_ZEC', results[0].btczec, balances.ZEC, 1, 0, 0);
-                        }
-                        return null;
-                    })
-                    .then((result) => {
-                        mysql_modul.update_choice(results[0].id);
-                        console.log(result);
-                    })
-                    .catch((err) => {
-                        console.log(err.message);
-                    });
-            } else {
-                console.log('jetzt wird nicht gehandelt');
+                return exchange.buy('ETH_ZEC', results[0].ethzec, (balances.ETH / results[0].ethzec), 1, 0, 0)
+                    .then(() => exchange.sell('BTC_ZEC', results[0].btczec, balances.ZEC, 1, 0, 0));
+            } else if (balances.ZEC > 0.05) {
+                return exchange.sell('BTC_ZEC', results[0].btczec, balances.ZEC, 1, 0, 0);
             }
+            return null;
+        })
+        .then((result) => {
+            mysql_modul.update_choice(results[0].id);
+            console.log(result);
+        })
+        .catch((err) => {
+            console.log(err.message);
         });
 }, null, true);
+
+function workToDo(result) {
+    if (result[0].status == 0)
+        return Promise.resolve(result);
+    else
+        return Promise.reject(Error('NOTHING_TO_DO'));
+}
