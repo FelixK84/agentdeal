ad205dd3700412dde9515696fde0935ec0364a44
Sven <sven@mutzl.org>
2017-12-02 15:26:55 +0000

    Timer parameter

--- module/timer.js
+++ module/timer.js
@@ -1,5 +1,5 @@
 const CronJob = require('cron').CronJob;
 
-module.exports=(interval,func)=>{
-    return new CronJob(interval, func);
+module.exports=(interval,func,p,q)=>{
+    return new CronJob(interval, func, p, q);
 };
